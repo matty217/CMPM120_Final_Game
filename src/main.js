3869b98d28aa0a3aed0a67c5fe8df46c29071a9b
“Mrinmoyee <mrmishra@ucsc.edu>
2022-05-10 22:29:30 -0700

    added scene switcher to the other levels

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ let config = {
             debug: true
         }
     },
-    scene: [ Level1, Level3]
+    scene: [ Level1, Level2, Level3, Level4, Level5 ]
 }
 
 
@@ -28,16 +28,51 @@ let sceneSwitcher = (event) => {
     //console.log("Key is: " + event.key);
     switch(event.key) {
         case '1':
-            console.log('check1');
             game.scene.start('level1Scene');
             game.scene.bringToTop('level1Scene');
+            game.scene.pause('level2Scene');
             game.scene.pause('level3Scene');
+            game.scene.pause('level4Scene');
+            game.scene.pause('level5Scene');
+            
+            break;
+
+        case '2':
+            game.scene.start('level2Scene');
+            game.scene.bringToTop('level2Scene');
+            game.scene.pause('level1Scene');
+            game.scene.pause('level3Scene');
+            game.scene.pause('level4Scene');
+            game.scene.pause('level5Scene');
+
             break;
         case '3':
-            console.log('check2');
             game.scene.start('level3Scene');
             game.scene.bringToTop('level3Scene');
             game.scene.pause('level1Scene');
+            game.scene.pause('level2Scene');
+            game.scene.pause('level4Scene');
+            game.scene.pause('level5Scene');
+
+            break;
+
+        case '4':
+            game.scene.start('level4Scene');
+            game.scene.bringToTop('level4Scene');
+            game.scene.pause('level1Scene');
+            game.scene.pause('level2Scene');
+            game.scene.pause('level3Scene');
+            game.scene.pause('level5Scene');
+
+            break;
+
+        case '5':
+            game.scene.start('level5Scene');
+            game.scene.bringToTop('level5Scene');
+            game.scene.pause('level1Scene');
+            game.scene.pause('level2Scene');
+            game.scene.pause('level3Scene');
+            game.scene.pause('level4Scene');
 
             break;
         default:
---
+++ src/scenes/Level_5.js
@@ -0,0 +1,52 @@
+class Level5 extends Phaser.Scene {
+    constructor() {
+        super("level5Scene");
+    }
+
+    preload() {
+        this.load.image('rect', './assets/white-square.png');
+
+    }
+
+    create() {
+        this.ACCELERATION = 500;
+        this.MAX_X_VEL = 500;   // pixels/second
+        this.MAX_Y_VEL = 5000;
+        this.DRAG = 600;    // DRAG < ACCELERATION = icy slide
+        this.physics.world.gravity.y = 3000;
+
+        this.cameras.main.setBackgroundColor('#0000ff');
+
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+
+
+        //this.background = this.add.rectangle(game.config.width/2, game.config.height/2, game.config.width, game.config.height, 0x444444).setOrigin(0.5,0.5);
+
+        this.player = new Cat(this, game.config.width/2, game.config.height/2, 'rect', 0).setOrigin(0.5, 0.5).setScale(0.2);
+
+        this.ground = this.add.sprite(game.config.width/2, game.config.height-100, 'rect', 0).setOrigin(0.5,0).setScale(10);
+        this.wall1 = this.add.sprite(game.config.width-100, game.config.height/2, 'rect', 0).setOrigin(0, 0.5).setScale(10);
+
+        this.platformGroup = this.physics.add.group( {allowGravity: false, immovable: true } );
+        this.physics.add.collider(this.player, this.platformGroup);
+
+        this.platformGroup.add(this.ground);
+        this.platformGroup.add(this.wall1);
+
+        this.input.keyboard.on('keydown', sceneSwitcher);  
+
+        var style = { font: "20px Arial", fill: "#ffffff" };
+        this.add.text(100,100,'level 5', style)
+    }
+
+    update() {
+        this.player.update();
+
+    }
+
+}
