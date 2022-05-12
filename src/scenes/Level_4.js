2c14eba0839d444e540227ff73f9c1c0e3e92d45
“Mrinmoyee <mrmishra@ucsc.edu>
2022-05-11 19:50:42 -0700

    added level scroll when player moves (level 3,4,5)

--- src/scenes/Level_4.js
+++ src/scenes/Level_4.js
@@ -9,6 +9,11 @@ class Level4 extends Phaser.Scene {
     }
 
     create() {
+        // camera and world bounds
+        // (change static values to a variable later)
+        this.cameras.main.setBounds(0, 0, 2000 , 720);
+        this.physics.world.setBounds(0, 0,2000, 720);
+
         this.ACCELERATION = 500;
         this.MAX_X_VEL = 500;   // pixels/second
         this.MAX_Y_VEL = 5000;
@@ -30,18 +35,24 @@ class Level4 extends Phaser.Scene {
         this.player = new Cat(this, game.config.width/2, game.config.height/2, 'rect', 0).setOrigin(0.5, 0.5).setScale(0.2);
 
         this.ground = this.add.sprite(game.config.width/2, game.config.height-100, 'rect', 0).setOrigin(0.5,0).setScale(10);
-        this.wall1 = this.add.sprite(game.config.width-100, game.config.height/2, 'rect', 0).setOrigin(0, 0.5).setScale(10);
+        this.wall1 = this.add.sprite(game.config.width+1000, game.config.height/2, 'rect', 0).setOrigin(0, 0.5).setScale(10);
+        this.wall2 = this.add.sprite(0, game.config.height/2, 'rect', 0).setOrigin(0, 0).setScale(0.5);
+
 
         this.platformGroup = this.physics.add.group( {allowGravity: false, immovable: true } );
         this.physics.add.collider(this.player, this.platformGroup);
 
         this.platformGroup.add(this.ground);
         this.platformGroup.add(this.wall1);
+        this.platformGroup.add(this.wall2);
 
         this.input.keyboard.on('keydown', sceneSwitcher);  
 
         var style = { font: "20px Arial", fill: "#ffffff" };
         this.add.text(100,100,'level 4', style)
+
+        // follow player with camera
+        this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
     }
 
     update() {
