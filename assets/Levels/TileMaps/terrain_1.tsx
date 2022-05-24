924c80ed0caa4316dd5b8b2534f4544c6399eba4
matty217 <mathill@ucsc.edu>
2022-05-24 14:05:45 -0700

    making terrain tilemap

---
+++ assets/Levels/TileMaps/terrain_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="terrain_1" tilewidth="512" tileheight="512" tilecount="20" columns="5">
+ <image source="../../../../../../../Downloads/terrain_tiles.png" width="2560" height="2048"/>
+</tileset>
