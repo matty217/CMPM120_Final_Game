f9b43ada9312b2ab674b28d72e1dad11b3ff5c85
matty217 <mathill@ucsc.edu>
2022-05-24 15:17:17 -0700

    tile fixes

--- assets/Levels/TileMaps/terrain_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="terrain_1" tilewidth="512" tileheight="512" tilecount="24" columns="6">
- <image source="../../../../../../../Downloads/terrain_tiles (1).png" width="3072" height="2048"/>
-</tileset>
