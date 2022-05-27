e95bca5507c9797f708f88839bba5089c8edca95
matty217 <mathill@ucsc.edu>
2022-05-27 14:04:17 -0700

    big additions

--- src/prefabs/Cat.js
+++ src/prefabs/Cat.js
@@ -21,6 +21,13 @@ class Cat extends Phaser.Physics.Arcade.Sprite {
         this.SLIDE_VEL = 1500;
         this.addedXVelocity = 0;
         this.hurt = false;
+        this.alive = true;
+
+        this.anim_walking = false;
+        this.anim_jumping = false;
+        this.anim_climb = false;
+        this.anim_slide = false;
+        this.anim_idle = false;
     }
 
     create() {
@@ -37,134 +44,162 @@ class Cat extends Phaser.Physics.Arcade.Sprite {
         //console.log((this.onGround == false && (this.body.blocked.right || this.body.blocked.left)));
         //console.log((Phaser.Input.Keyboard.JustDown(keySPACE)));
 
-        if (this.wallSliding) {
-            this.body.setDragY(this.WALL_DRAG);
-            //this.body.setVelocityY(0)
-            this.body.allowGravity = false;
-            if (this.body.velocity.y < this.SLIDE_VEL) {
-                this.body.velocity.y += this.SLIDE_VEL/2;
+        if (this.alive) {
+
+            if (this.wallSliding) {
+                this.body.setDragY(this.WALL_DRAG);
+                //this.body.setVelocityY(0)
+                this.body.allowGravity = false;
+                if (this.body.velocity.y < this.SLIDE_VEL) {
+                    this.body.velocity.y += this.SLIDE_VEL/2;
+                }
+                this.play({ key : 'slide' });
+                this.anim_slide = true;
+                this.anim_idle = false;
+                this.anim_jumping = false;
+                this.anim_walking = false;
+            } else {
+                this.body.setDrag(0);
+                this.body.allowGravity = true;
             }
-        } else {
-            this.body.setDrag(0);
-            this.body.allowGravity = true;
+
+            if (this.onGround) {            //reset jump counter when on the ground
+                this.jumps = 0;
+            }
+
+    // STATE ON GROUND -----------------------------------------------------------------------
+            if (this.onGround)
+            {
+                this.anim_jumping = false;
+                //move left
+                if (keyA.isDown) {
+                    this.flipX = true;
+                    if (this.body.velocity.x > -this.MAX_GROUND_VEL) {
+                        this.body.velocity.x -= this.ADDED_VEL;
+                    }
+                    if (!this.anim_walking) {
+                        this.play({ key : 'walk' });
+                        this.anim_walking = true;
+                    }
+                }
+                //move right
+                if (keyD.isDown) {
+                    this.flipX = false;
+                    if (this.body.velocity.x < this.MAX_GROUND_VEL) {
+                        this.body.velocity.x += this.ADDED_VEL;
+                    }
+                    if (!this.anim_walking) {
+                        this.play({ key : 'walk' });
+                        this.anim_walking = true;
+                    }
+                }
+                //stop moving when no input
+                if ((!keyA.isDown && !keyD.isDown)||(keyA.isDown && keyD.isDown)) {
+                    this.body.setDragX(this.DRAG);
+                    //this.addedXVelocity
+
+                }
+
+                // jump
+                if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+                    // normal jump
+                    this.body.setVelocityY(this.JUMP_VEL);
+                    console.log('Jump');
+                    if (!this.anim_jumping) {
+                        this.play({ key : 'jump' });
+                        this.anim_jumping = true;
+                        this.anim_walking = false;
+                    }
+                }
+            }
+
+    // STATE IN AIR -------------------------------------------------------------------------
+            else if (!this.onGround && !this.wallSliding)
+            {
+                //move left
+                if (keyA.isDown) {
+                    this.flipX = true;
+                    if (this.body.velocity.x > -this.MAX_GROUND_VEL) {
+                        this.body.velocity.x -= this.ADDED_VEL*(3/4);
+                    } else {
+                    }
+                }
+                //move right
+                if (keyD.isDown) {
+                    this.flipX = false;
+                    if (this.body.velocity.x < this.MAX_GROUND_VEL) {
+                        this.body.velocity.x += this.ADDED_VEL*(3/4);
+                    }
+                }
+                //stop moving when no input
+                if ((!keyA.isDown && !keyD.isDown)||(keyA.isDown && keyD.isDown)) {
+                    this.body.setDragX(this.AIR_DRAG);
+                    //this.addedXVelocity
+
+                }
+
+                // jump
+                if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+                    // double jump
+                    if (this.jumps < this.maxAirJumps) {
+                        this.body.setVelocityY(this.AIR_JUMP_VEL);
+                        console.log('Air Jump');
+                        this.jumps += 1;
+                    }
+                }
+            }
+
+    // STATE WALL SLIDING ---------------------------------------------------------------------------
+            else if (this.wallSliding)
+            {
+                // CLIMBING ---------------------
+                if (keyW.isDown) {
+                    console.log('CLIMBING');
+                    this.body.velocity.y = this.CLIMB_VEL;
+                }
+                //move left
+                if (keyA.isDown) {
+                    if (this.body.velocity.x > -this.MAX_GROUND_VEL) {
+                        this.body.velocity.x -= this.ADDED_VEL*(3/4);
+                    } else {
+                    }
+                }
+                //move right
+                if (keyD.isDown) {
+                    if (this.body.velocity.x < this.MAX_GROUND_VEL) {
+                        this.body.velocity.x += this.ADDED_VEL*(3/4);
+                    }
+                }
+                //stop moving when no input
+                if ((!keyA.isDown && !keyD.isDown)||(keyA.isDown && keyD.isDown)) {
+                    this.body.setDragX(this.DRAG);
+                    if (this.body.blocked.right) {
+                        this.body.setVelocityX(this.ADDED_VEL);
+                    }
+                    else if (this.body.blocked.left) {
+                        this.body.setVelocityX(-this.ADDED_VEL);
+                    }
+                }
+
+                if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+                    // wall jump
+                    this.body.setVelocityY(this.WALL_JUMP_VEL_Y);
+                    if (this.body.blocked.right) {
+                        this.body.setVelocityX(-this.WALL_JUMP_VEL_X);
+                    }
+                    else if (this.body.blocked.left) {
+                        this.body.setVelocityX(this.WALL_JUMP_VEL_X);
+                    }
+                    console.log('Wall Jump');
+                }
+                
+            }
+            if (Phaser.Input.Keyboard.JustDown(keyQ)) {
+                game.sound.play('meow');
+            }    
         }
-
-        if (this.onGround) {            //reset jump counter when on the ground
-            this.jumps = 0;
+        else {
+            this.setVelocity(0,0);
         }
-
-// STATE ON GROUND -----------------------------------------------------------------------
-        if (this.onGround)
-        {
-            //move left
-            if (keyA.isDown) {
-                if (this.body.velocity.x > -this.MAX_GROUND_VEL) {
-                    this.body.velocity.x -= this.ADDED_VEL;
-                } else {
-                }
-            }
-            //move right
-            if (keyD.isDown) {
-                if (this.body.velocity.x < this.MAX_GROUND_VEL) {
-                    this.body.velocity.x += this.ADDED_VEL;
-                }
-            }
-            //stop moving when no input
-            if ((!keyA.isDown && !keyD.isDown)||(keyA.isDown && keyD.isDown)) {
-                this.body.setDragX(this.DRAG);
-                //this.addedXVelocity
-
-            }
-
-            // jump
-            if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-                // normal jump
-                this.body.setVelocityY(this.JUMP_VEL);
-                console.log('Jump');
-            }
-        }
-
-// STATE IN AIR -------------------------------------------------------------------------
-        else if (!this.onGround && !this.wallSliding)
-        {
-            //move left
-            if (keyA.isDown) {
-                if (this.body.velocity.x > -this.MAX_GROUND_VEL) {
-                    this.body.velocity.x -= this.ADDED_VEL*(3/4);
-                } else {
-                }
-            }
-            //move right
-            if (keyD.isDown) {
-                if (this.body.velocity.x < this.MAX_GROUND_VEL) {
-                    this.body.velocity.x += this.ADDED_VEL*(3/4);
-                }
-            }
-            //stop moving when no input
-            if ((!keyA.isDown && !keyD.isDown)||(keyA.isDown && keyD.isDown)) {
-                this.body.setDragX(this.AIR_DRAG);
-                //this.addedXVelocity
-
-            }
-
-            // jump
-            if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-                // double jump
-                if (this.jumps < this.maxAirJumps) {
-                    this.body.setVelocityY(this.AIR_JUMP_VEL);
-                    console.log('Air Jump');
-                    this.jumps += 1;
-                }
-            }
-        }
-
-// STATE WALL SLIDING ---------------------------------------------------------------------------
-        else if (this.wallSliding)
-        {
-            // CLIMBING ---------------------
-            if (keyW.isDown) {
-                console.log('CLIMBING');
-                this.body.velocity.y = this.CLIMB_VEL;
-            }
-            //move left
-            if (keyA.isDown) {
-                if (this.body.velocity.x > -this.MAX_GROUND_VEL) {
-                    this.body.velocity.x -= this.ADDED_VEL*(3/4);
-                } else {
-                }
-            }
-            //move right
-            if (keyD.isDown) {
-                if (this.body.velocity.x < this.MAX_GROUND_VEL) {
-                    this.body.velocity.x += this.ADDED_VEL*(3/4);
-                }
-            }
-            //stop moving when no input
-            if ((!keyA.isDown && !keyD.isDown)||(keyA.isDown && keyD.isDown)) {
-                this.body.setDragX(this.DRAG);
-                if (this.body.blocked.right) {
-                    this.body.setVelocityX(this.ADDED_VEL);
-                }
-                else if (this.body.blocked.left) {
-                    this.body.setVelocityX(-this.ADDED_VEL);
-                }
-            }
-
-            if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-                // wall jump
-                this.body.setVelocityY(this.WALL_JUMP_VEL_Y);
-                if (this.body.blocked.right) {
-                    this.body.setVelocityX(-this.WALL_JUMP_VEL_X);
-                }
-                else if (this.body.blocked.left) {
-                    this.body.setVelocityX(this.WALL_JUMP_VEL_X);
-                }
-                console.log('Wall Jump');
-            }
-            
-        }
-        if (Phaser.Input.Keyboard.JustDown(keyQ)) {
-            game.sound.play('meow');
-        }    
     }
 }
--- src/scenes/Level_1.js
+++ src/scenes/Level_1.js
@@ -9,35 +9,102 @@ class Level1 extends Phaser.Scene {
         this.load.image('platform', './assets/platform.png');
         this.load.image('platform_vert', './assets/platform_vert.png');
         this.load.image('spikes', './assets/spikes.png');
-        this.load.image('boulder', './assets/Levels/Blocks/1x1 Boulder.PNG');
+        this.load.image('boulder', './assets/Levels/Blocks/Tiles/1x1 Boulder.PNG');
         this.load.image('block_1', './assets/Levels/Blocks/1x1 Block.PNG');
         this.load.image('plat_1', './assets/Levels/Blocks/Platform Large.PNG');
         this.load.image('plat_2', './assets/Levels/Blocks/Platform Small.PNG');
-        this.load.image('spikes_D', './assets/Levels/Blocks/1x1 Spikes.PNG');
-        this.load.image('spikes_R', './assets/Levels/Blocks/1x1 Spikes_R.PNG');
-        this.load.image('spikes_L', './assets/Levels/Blocks/1x1 Spikes_L.PNG');
-        this.load.image('spikes_U', './assets/Levels/Blocks/1x1 Spikes_U.PNG');
+        this.load.image('spikes_D', './assets/Levels/Blocks/Tiles/1x1 Spikes.PNG');
         this.load.image('back_1', './assets/Levels/Level-1/Background-1.PNG');
         this.load.image('back_2', './assets/Levels/Level-1/Midground-1.PNG');
         this.load.image('back_3', './assets/Levels/Level-1/Foreground-1.PNG');
         this.load.image('back_4', './assets/Levels/Level-1/back_brown.png');
         this.load.image('4x1', './assets/Levels/Blocks/4x1 Block_f.PNG');
         this.load.image('1x4', './assets/Levels/Blocks/4x1 Block_Rf.PNG');
+        this.load.image('smoke', './assets/Cat/smoke.png');
 
         // TILE MAP
         this.load.image('terrain_tiles', 'assets/Levels/TileMaps/terrain_tiles.png');
         this.load.tilemapTiledJSON('platform_map', 'assets/Levels/TileMaps/Level1.json');
 
 
-        this.load.spritesheet('cat', 'assets/Cat/cat_walk_sheet.png', {
+        this.load.spritesheet('cat_walk', 'assets/Player/Walk_Sheet.png', {
             frameWidth: 512,
             frameHeight: 512
         });
+        this.load.spritesheet('cat_jump', 'assets/Player/Jump_Sheet.png', {
+            frameWidth: 512,
+            frameHeight: 512
+        });
+        this.load.spritesheet('cat_climb', 'assets/Player/Climb_Sheet.png', {
+            frameWidth: 512,
+            frameHeight: 512
+        });
+        this.load.spritesheet('cat_slide', 'assets/Player/Slide_Sheet.png', {
+            frameWidth: 512,
+            frameHeight: 512
+        });
+
         this.load.audio('meow', 'assets/meow.wav');
 
     }
 
     create() {
+
+        //ANIMATIONS
+        const catWalk = this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNames('cat_walk', {
+                start: 0,
+                end: 11
+            }),
+            frameRate: 16,
+            repeat: -1
+        });
+
+        const catJump = this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNames('cat_jump', {
+                start: 0,
+                end: 5
+            }),
+            frameRate: 22,
+            repeat: 0
+        });
+
+        const catClimb = this.anims.create({
+            key: 'climb',
+            frames: this.anims.generateFrameNames('cat_climb', {
+                start: 0,
+                end: 4
+            }),
+            frameRate: 16,
+            repeat: -1
+        });
+
+        const catSlide = this.anims.create({
+            key: 'slide',
+            frames: this.anims.generateFrameNames('cat_slide', {
+                start: 0,
+                end: 0
+            }),
+            frameRate: 0,
+            repeat: -1
+        });
+
+        const catIdle = this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNames('cat_walk', {
+                start: 0,
+                end: 0
+            }),
+            frameRate: 16,
+            repeat: -1
+        });
+
+
+
+
+
         // BACKGROUND STUFF
         //this.background = this.add.rectangle(game.config.width/2, game.config.height/2, game.config.width, game.config.height, 0x444444).setOrigin(0.5,0.5);
         this.back_0001 = this.add.sprite(-1000, 2500, 'back_1', 0).setScale(3).setScrollFactor(0.2,0.2);
@@ -93,12 +160,10 @@ class Level1 extends Phaser.Scene {
 
 
             // set up player character
-        this.player = new Cat(this, -5000, 12000, 'cat', 0).setOrigin(0.5, 0.5).setScale(1);
+        this.player = new Cat(this, -5000, 12000, 'cat_walk', 0).setOrigin(0.5, 0.5).setScale(1);
+        this.respawnX = -4000;
+        this.respawnY = 13000;
         //this.player.body.setMaxVelocity(600, 5000);
-
-        this.cat_example = this.add.sprite(2100, -750, 'cat', 0).setOrigin(0.5,0.5).setScale(0.5);
-
-
         
     
         // TILE MAP COLLIDER
@@ -108,6 +173,49 @@ class Level1 extends Phaser.Scene {
 
         this.physics.add.collider(this.player, groundLayer);
         groundLayer.setCollisionBetween(0,23);
+
+        // SPIKE GROUP
+        this.spikes = map.createFromObjects("Objects", {
+            name: "Spike",
+            key: "spikes_D",
+            frame: "0"
+        });
+
+        this.physics.world.enable(this.spikes, Phaser.Physics.Arcade.STATIC_BODY);
+        this.spikeGroup = this.add.group(this.spikes);
+
+        this.physics.add.overlap(this.player, this.spikeGroup, (obj1, obj2) => {
+            this.Death(obj1, obj2);
+        })
+
+        // RESPAWN POINT GROUP
+        this.respawnPoint = map.createFromObjects("Objects", {
+            name: "Respawn",
+            key: "",
+            frame: ""
+        });
+
+        this.physics.world.enable(this.respawnPoint, Phaser.Physics.Arcade.STATIC_BODY);
+        this.respawnGroup = this.add.group(this.respawnPoint);
+
+        this.physics.add.overlap(this.player, this.respawnGroup, (obj1, obj2) => {
+            this.respawnX = obj2.x;
+            this.respawnY = obj2.y + 1500;
+        })
+
+        // FALL DEATH GROUP
+        this.deathPoint = map.createFromObjects("Objects", {
+            name: "Death",
+            key: "",
+            frame: ""
+        });
+
+        this.physics.world.enable(this.deathPoint, Phaser.Physics.Arcade.STATIC_BODY);
+        this.deathGroup = this.add.group(this.deathPoint);
+
+        this.physics.add.overlap(this.player, this.deathGroup, (obj1, obj2) => {
+            this.Death(obj1);
+        })
 
        
 
@@ -130,19 +238,6 @@ class Level1 extends Phaser.Scene {
         this.add.text(13800,-2400,'END', style);
 
 
-
-        //ANIMATIONS
-        const catWalk = this.anims.create({
-            key: 'walk',
-            frames: this.anims.generateFrameNames('cat', {
-                start: 0,
-                end: 11
-            }),
-            frameRate: 18,
-            repeat: -1
-        });
-
-        this.cat_example.play({ key: 'walk' });
     }
 
     update() {
@@ -168,19 +263,42 @@ class Level1 extends Phaser.Scene {
         game.scene.pause('level5Scene');
     }
 
-    spikeHurt(player, spike) {
-        if (!player.hurt) {
-            player.hurt = true;
-            this.cameras.main.shake(100);
-            if (this.player.hurt == true) {
-                let unHurt = this.time.addEvent({ delay: 1000, callback: () =>{
-                    this.unHurt();
+    Death(player, spike) {
+        if (this.player.alive == true) {
+            player.alive = false;
+            this.cameras.main.shake(500);
+            this.deathParticles = this.add.particles('smoke');
+            this.partEm = this.deathParticles.createEmitter({
+                // frame: 'yellow',
+                radial: true,
+                // x: this.newCannon.x + 100,
+                // y: this.newCannon.y,
+                lifespan: 2000,
+                speed: { min: 400, max: 800 },
+                quantity: 300,
+                gravityY: 0,
+                scale: { start: 4, end: 0, ease: 'Power3' },
+                active: true,
+                
+                follow: player
+            });
+            this.partEm.explode(50, this.player.x, this.player.y);
+            this.player.alpha = 0;
+
+            let fadeout = this.time.addEvent({ delay: 1200, callback: () =>{
+                this.cameras.main.fadeOut(500);
+
+                let respawn = this.time.addEvent({ delay: 500, callback: () =>{
+                    this.Respawn();
                 }});
-            }
+            }});
         }
     }
 
-    unHurt() {
-        this.player.hurt = false;
+    Respawn() {
+        this.cameras.main.fadeIn(800);
+        this.player.setPosition(this.respawnX, this.respawnY);
+        this.player.alive = true;
+        this.player.alpha = 1;
     }
 }
